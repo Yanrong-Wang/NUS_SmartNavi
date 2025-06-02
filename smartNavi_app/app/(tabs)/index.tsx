460f424b3de1979d72658c97152d31490b6e0c59
Yanrong-Wang <e1485168@u.nus.edu>
2025-06-03 06:42:50 +0800

    Create Homescreen, move index to homescreen

--- smartNavi_app/app/(tabs)/index.tsx
+++ smartNavi_app/app/(tabs)/index.tsx
@@ -4,109 +4,47 @@ import { Picker } from '@react-native-picker/picker';
 import { Platform } from 'react-native';
 import RouteSearch from '@/components/Route_Search';
 import busRoutes from '@/constants/busRoutes';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import schedule from '@/components/schedule';
 
-const STOPS = [
-  "Central Library",
-  "Yusof Ishak House",
-  "Univeristy Hall",
-  "Opp University Hall",
-  "LT27",
-  "S17",
-  "Kent Ridge MRT",
-  "Opp Kent Ridge MRT",
-  "University Town",
-  "Prince George's Park Foyer",
-  "Prince George's Park",
-  "Opp HSSML",
-  "BIZ2",
-  "COM3",
-];
-
-export default function App() {
-  const [from, setFrom] = useState('');
+const [from, setFrom] = useState('');
   const [to, setTo] = useState('');
   const [error, setError] = useState('');
-
-  function handleOrigin(value: string) {
-    setFrom(value);
-    if (value === to && value) {
-      setError('Origin and destination cannot be the same!');
-    } else {
-      setError('');
-    }
-  }
-
-  function handleDestination(value: string) {
-    setTo(value);
-    if (from === value && value) {
-      setError('Origin and destination cannot be the same!');
-    } else {
-      setError('');
-    }
-  }
-
+  const [schedule, setSchedule] = useState('');
+  
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>NUS SmartNavi: Pick Route</Text>
-      <View style={styles.pickerContainer}>
-        <Text>Origin:</Text>
-        <Picker
-          selectedValue={from}
-          onValueChange={handleOrigin}
-          style={Platform.OS === 'android' ? styles.picker : undefined}
-        >
-          <Picker.Item label="Select" value="" />
-          {STOPS.map(stop => (
-            (stop === from || stop !== to) && (
-              <Picker.Item key={stop} label={stop} value={stop} />
-            )
-          ))}
-        </Picker>
-      </View>
-
-      <View style={styles.pickerContainer}>
-        <Text>Destination:</Text>
-        <Picker
-          selectedValue={to}
-          onValueChange={handleDestination}
-          style={Platform.OS === 'android' ? styles.picker : undefined}
-        >
-          <Picker.Item label="Select" value="" />
-          {STOPS.map(stop => (
-            (stop === to || stop !== from) && (
-              <Picker.Item key={stop} label={stop} value={stop} />
-            )
-          ))}
-        </Picker>
-      </View>
-
-      {error.length > 0 && (
-        <Text style={styles.error}>{error}</Text>
-      )}
-
-      <RouteSearch from={from} to={to} disabled={!!error || !from || !to} />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name = "Home">
+          {Props => (
+            <HomeScreen
+              {...Props}
+              from = {from}
+              setFrom = {setFrom}
+              to = {to}
+              setTo = {setTo}
+              error = {error}
+              setError = {setError}
+              schedule = {schedule}
+              setSchedule = {setSchedule}
+            />
+          )}
+        </Stack.Screen>
+        <Stack.Screen name = "schedule">
+          {props => (
+            <ScheduleScreen>
+              {...props}
+              schedule = {schedule}
+              setSchedule = {setSchedule}
+              setTo = {setTo}
+            />
+          )}
+        </Stack.Screen)
+      </Stack.Navigator>
+    </NavigationContainer>
   );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    marginTop: 40,
-    padding: 30,
-  },
-  heading: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  pickerContainer: {
-    marginBottom: 20,
-  },
-  picker: {
-    height: 50,
-  },
-  error: {
-    color: 'red',
-    marginBottom: 10,
-  },
-});
+
+const Stack = createStackNavigator()
+
---
+++ smartNavi_app/components/HomeScreen.tsx
@@ -0,0 +1,116 @@
+import React, { useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { Picker } from '@react-native-picker/picker';
+import { Platform } from 'react-native';
+import RouteSearch from '@/components/Route_Search';
+import busRoutes from '@/constants/busRoutes';
+
+const STOPS = [
+  "Central Library",
+  "Yusof Ishak House",
+  "Univeristy Hall",
+  "Opp University Hall",
+  "LT27",
+  "S17",
+  "Kent Ridge MRT",
+  "Opp Kent Ridge MRT",
+  "University Town",
+  "Prince George's Park Foyer",
+  "Prince George's Park",
+  "Opp HSSML",
+  "BIZ2",
+  "COM3",
+];
+
+// usestate is extracted to the App level 
+// use props to pass down state
+export default function HomeScreen({
+    to, setTo, from , setFrom, error, setError
+}){
+    function handleOrigin(value: string) {
+    setFrom(value);
+    if (value === to && value) {
+      setError('Origin and destination cannot be the same!');
+    } else {
+      setError('');
+    }
+  }
+
+  function handleDestination(value: string) {
+    setTo(value);
+    if (from === value && value) {
+      setError('Origin and destination cannot be the same!');
+    } else {
+      setError('');
+    }
+  }
+
+  // Display main page: origin and destination selection
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>NUS SmartNavi: Pick Route</Text>
+      <View style={styles.pickerContainer}>
+        <Text>Origin:</Text>
+        <Picker
+          selectedValue={from}
+          onValueChange={handleOrigin}
+          style={Platform.OS === 'android' ? styles.picker : undefined}
+        >
+          <Picker.Item label="Select" value="" />
+          {STOPS.map(stop => (
+            (stop === from || stop !== to) && (
+              <Picker.Item key={stop} label={stop} value={stop} />
+            )
+          ))}
+        </Picker>
+      </View>
+
+      <View style={styles.pickerContainer}>
+        <Text>Destination:</Text>
+        <Picker
+          selectedValue={to}
+          onValueChange={handleDestination}
+          style={Platform.OS === 'android' ? styles.picker : undefined}
+        >
+          <Picker.Item label="Select" value="" />
+          {STOPS.map(stop => (
+            (stop === to || stop !== from) && (
+              <Picker.Item key={stop} label={stop} value={stop} />
+            )
+          ))}
+        </Picker>
+      </View>
+
+      {error.length > 0 && (
+        <Text style={styles.error}>{error}</Text>
+      )}
+
+      <RouteSearch from={from} to={to} disabled={!!error || !from || !to} />
+    </View>
+  );
+}
+  
+const styles = StyleSheet.create({
+  container: {
+    marginTop: 40,
+    padding: 30,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  pickerContainer: {
+    marginBottom: 20,
+  },
+  picker: {
+    height: 50,
+  },
+  error: {
+    color: 'red',
+    marginBottom: 10,
+  },
+});
+  
+
+  
