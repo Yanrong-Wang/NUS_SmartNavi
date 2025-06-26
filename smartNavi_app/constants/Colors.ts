d025dd65b80b37b7f6d246b8a00cdf5f1102095e
Yanrong-Wang <e1485168@u.nus.edu>
2025-06-26 20:59:17 +0800

    Dark color scheme

--- smartNavi_app/constants/Colors.ts
+++ smartNavi_app/constants/Colors.ts
@@ -17,7 +17,7 @@ export const Colors = {
   },
   dark: {
     text: '#ECEDEE',
-    background: '#fff',
+    background: '#1C1C1E',
     tint: tintColorDark,
     icon: '#9BA1A6',
     tabIconDefault: '#9BA1A6',
